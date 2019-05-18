2903764eeca20e615c643cf8f3963976c5b86b50
Marc Poppy <poppy.marc@gmail.com>
2019-05-18 20:03:40 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(dwarves)# code an argument here
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1} #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |calls| calls.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(short_words)
+  #check if any in short words is > 4
+  if short_words.find { |word| word.length > 4}
+    true
+  else
+    false
+  end
 end
 
-def find_the_cheese# code an argument here
+#accept an array of strings, find and return the first string that is a type of cheese
+def find_the_cheese(snacks)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  #cheese = cheese_types.join(', ')
+  snacks.find { |snack| cheese_types.include?(snack) } #returns true if there is a match
+  #now that we know there is a match, how do we capture it
 end
